39576b0bd290b174a946438d12277f62b5d867f5
merchandev <merchan.dev@hotmail.com>
2026-08-04 11:11:33 -0400

    fix: next.config.ts syntax error

--- next.config.ts
+++ next.config.ts
@@ -36,6 +36,7 @@ const nextConfig: NextConfig = {
             key: 'Strict-Transport-Security',
             value: 'max-age=63072000; includeSubDomains; preload',
           },
+        ],
       },
     ];
   },
