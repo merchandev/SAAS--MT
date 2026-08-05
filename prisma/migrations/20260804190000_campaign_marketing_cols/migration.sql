d95ad2a92bbc5f5bc6c8a599f201a161e3fc99e3
merchandev <merchan.dev@hotmail.com>
2026-08-04 22:59:20 -0400

    fix(prisma): add missing marketing columns to EmailCampaign

---
+++ prisma/migrations/20260804190000_campaign_marketing_cols/migration.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "EmailCampaign" ADD COLUMN "marketingListId" TEXT;
+ALTER TABLE "EmailCampaign" ADD COLUMN "marketingSegmentId" TEXT;
+ALTER TABLE "EmailCampaign" ADD COLUMN "emailTemplateId" TEXT;
